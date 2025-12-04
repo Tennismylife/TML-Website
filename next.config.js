de4f311ba5d70ed58b107d5685fc0afaca627586
Tennismylife <49320517+Tennismylife@users.noreply.github.com>
2025-12-04 12:34:24 +0100

    Update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, // Abilita React Strict Mode
   experimental: {
-    appDir: true,
+    appDir: true, // Abilita la nuova app directory
   },
-  output: 'standalone',
+  distDir: '.next_build', // Cartella build personalizzata
+  output: 'standalone', // ? Necessario per deploy su VPS
 };
 
 module.exports = nextConfig;
