59f28f13bdd28dee7167be532a91bd61cdc452b0
Tennismylife <49320517+Tennismylife@users.noreply.github.com>
2026-05-01 18:35:57 +0200

    Fix

--- app/players/[id]/clay/page.tsx
+++ app/players/[id]/clay/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import type { Metadata } from 'next';
 import SurfacePageContent, { generateSurfaceMetadata } from '../surfacePageFactory';
 
+export const dynamic = 'force-dynamic';
+
 export async function generateMetadata({ params }: any): Promise<Metadata> {
   const { id } = await params;
   return generateSurfaceMetadata(id, 'Clay');
